d6ea8e83e8bd9f8c34ac91e68af946f4e797f78a
Ashlee Campaz <ashleecampaz@unicauca.edu.co>
2024-09-16 00:18:12 -0500

    GUI de inicio de sesion y registro

---
+++ src/main/java/com/easyconference/gestion_conferencias/Gestion_conferencias.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.easyconference.gestion_conferencias;
+
+import com.easyconference.presentation.GUIlogin;
+import com.easyconference.presentation.GUIregister;
+
+/**
+ *
+ * @author Ashlee Campaz
+ */
+public class Gestion_conferencias {
+
+    public static void main(String[] args) {
+        GUIregister inicio = new GUIregister();
+        inicio.setVisible(true);
+    }
+}
